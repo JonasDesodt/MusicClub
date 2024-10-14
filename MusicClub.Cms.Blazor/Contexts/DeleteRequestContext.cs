3a7a8e80e9dc98850ad218dae36de6f33ae50468
JonasDesodt <95071893+JonasDesodt@users.noreply.github.com>
2024-10-14 09:34:59 +0200

    delete page styling

---
+++ MusicClub.Cms.Blazor/Contexts/DeleteRequestContext.cs
@@ -0,0 +1,9 @@
+﻿namespace MusicClub.Cms.Blazor.Contexts
+{
+    public class DeleteRequestContext<TDataResult> where TDataResult : class
+    {
+        public required TDataResult DataResult { get; set; }
+
+        public bool ShowImages { get; set; } = true;
+    }
+}
